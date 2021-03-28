e49b6186b7ad722e0bad713600d035b6fc5f82b6
RoaaMustafa <labushanab14@gmail.com>
2021-03-28 15:11:44 +0300

    add comments

--- Javascript/page.js
+++ Javascript/page.js
@@ -27,27 +27,27 @@ for(var i = 1 ; i <= animalNum; i++ )
     document.write(image); 
 
 
-/* if (favoriteAnimal === 'Cats') {
-    document.write ('<h3>' + 'Your Faorite Animal is :' +'</h3>');
-    document.write ('<img src ="http://4.bp.blogspot.com/-MzZCzWI_6Xc/UIUQp1qPfzI/AAAAAAAAHpA/OTwHCJSWFAY/s1600/cats_animals_kittens_cat_kitten_cute_desktop_1680x1050_hd-wallpaper-753974.jpeg">');
-}
-else if (favoriteAnimal === 'Dogs'){
-    document.write ('<h3>' + 'Your Faorite Animal is :' +'</h3>');
-document.write ('<img src ="https://th.bing.com/th/id/R4c645084ac66cc9c140856b8c5d20916?rik=orn21qtf%2bPLsXg&pid=ImgRaw"');   
-}
-else if (favoriteAnimal === 'Birds'){
-    document.write ('<h3>' + 'Your Faorite Animal is :' +'</h3>');
-    document.write ('<img src ="https://th.bing.com/th/id/OIP.OL9igW7Y6rUk-lusgXnOPAHaGZ?pid=ImgDet&rs=1"');
-}
-else {
-  alert  ('Welcome Again  '+ userName);
-
-}
-
-var yourInput = prompt ('Have you ever taken care of an animal? ');
-if (yourInput === 'yes') {
-alert ('So you are an animal lover' );
-}
-else{
-    alert ('Any way they are still cute ');
-} */
+/* 
+//if (favoriteAnimal === 'Cats') {
+//   document.write ('<h3>' + 'Your Faorite Animal is :' +'</h3>');
+//  document.write ('<img src ="http://4.bp.blogspot.com/-MzZCzWI_6Xc/UIUQp1qPfzI/AAAAAAAAHpA/OTwHCJSWFAY/s1600/cats_animals_kittens_cat_kitten_cute_desktop_1680x1050_hd-wallpaper-753974.jpeg">');
+//}
+//else if (favoriteAnimal === 'Dogs'){
+//    document.write ('<h3>' + 'Your Faorite Animal is :' +'</h3>');
+//document.write ('<img src ="https://th.bing.com/th/id/R4c645084ac66cc9c140856b8c5d20916?rik=orn21qtf%2bPLsXg&pid=ImgRaw"');   
+//}
+//else if (favoriteAnimal === 'Birds'){
+//   document.write ('<h3>' + 'Your Faorite Animal is :' +'</h3>');
+//    document.write ('<img src ="https://th.bing.com/th/id/OIP.OL9igW7Y6rUk-lusgXnOPAHaGZ?pid=ImgDet&rs=1"');
+//}
+//else {
+//  alert  ('Welcome Again  '+ userName);
+//
+//}
+//var yourInput = prompt ('Have you ever taken care of an animal? ');
+//if (yourInput === 'yes') {
+//alert ('So you are an animal lover' );
+//}
+//else{
+//    alert ('Any way they are still cute ');
+//} */
